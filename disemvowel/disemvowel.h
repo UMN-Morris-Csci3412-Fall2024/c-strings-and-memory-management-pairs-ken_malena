4d82650d35ac392e793d7aac3b2dcb0993f76d2b
mahon479 <mahon479@morris.umn.edu>
2024-09-26 11:12:41 -0500

    Reverted the disemvowel.h file to its original code since it should not be changed

--- disemvowel/disemvowel.h
+++ disemvowel/disemvowel.h
@@ -1,9 +1,6 @@
 #ifndef DISEMVOWEL_H_GUARD
 #define DISEMVOWEL_H_GUARD
 
-#include <stdbool.h> // Include stdbool.h for bool type
-
-bool is_vowel(char c);
 char *disemvowel(char *str);
 
-#endif // DISEMVOWEL_H_GUARD
+#endif
